c8db02655cf45e37cdf778e73930a26165e45368
Shrirambaabu <shrirambaabu0902@gmail.com>
2018-12-13 22:31:20 +0530

    Doing..

--- app/src/main/java/in/myreminder/srb/activity/AddReminderActivity.java
+++ app/src/main/java/in/myreminder/srb/activity/AddReminderActivity.java
@@ -1,6 +1,9 @@
 package in.myreminder.srb.activity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +22,7 @@ import java.util.Calendar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import in.myreminder.srb.AlarmBroadcaster;
 import in.myreminder.srb.R;
 import in.myreminder.srb.adapter.AlertAdapter;
 import in.myreminder.srb.database.DatabaseHelper;
@@ -156,4 +160,26 @@ public class AddReminderActivity extends AppCompatActivity implements SwipeRefre
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         }
     }
+
+
+    public void alarmSetter(Context context) {
+
+        //This will set the alarm time to be at the 14:30
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, 22);
+        calendar.set(Calendar.MINUTE, 27);
+
+        //This intent, will execute the AlarmBroadcaster when the alarm is triggered
+        Intent alertIntent = new Intent(context, AlarmBroadcaster.class);
+
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 1, alertIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+    }
+
+
 }
